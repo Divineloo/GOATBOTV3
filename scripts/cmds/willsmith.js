8d9074f2d4ace13bbdae955ef9865765f0dc0208
Divineloo <156040645+Divineloo@users.noreply.github.com>
2024-07-31 10:53:21 +0100

    Add files via upload

--- scripts/cmds/willsmith.js
+++ scripts/cmds/willsmith.js
@@ -20,21 +20,21 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, api, event, args, getText }) {
-
-	const axios = require('axios');
+      
+  const axios = require('axios');
 	const request = require('request');
 	const fs = require("fs");
 	 const { threadID, messageID, senderID, body } = event;
 	 let text = args.join(" ")
-	if (!text) return api.sendMessage('Please enter the correct format [text1 | text2 ]!', event.threadID, event.messageID);
-	const length_0 = parseInt(text.length)
-	const text1 = text.substr(0, text.indexOf("|")); 
-	if (!text1) return api.sendMessage('Please enter the correct format [text1 | text2 ]!', event.threadID, event.messageID);
-	const length = parseInt(text1.length)
-	const text2 = text.split("|").pop()
-	if (!text2) return api.sendMessage('Please enter the correct format [text1 | text2 ]!', event.threadID, event.messageID);
-	const length_2 = parseInt(text2.length)
-
+  if (!text) return api.sendMessage('Please enter the correct format [text1 | text2 ]!', event.threadID, event.messageID);
+  const length_0 = parseInt(text.length)
+  const text1 = text.substr(0, text.indexOf("|")); 
+  if (!text1) return api.sendMessage('Please enter the correct format [text1 | text2 ]!', event.threadID, event.messageID);
+  const length = parseInt(text1.length)
+  const text2 = text.split("|").pop()
+  if (!text2) return api.sendMessage('Please enter the correct format [text1 | text2 ]!', event.threadID, event.messageID);
+  const length_2 = parseInt(text2.length)
+  
 	 var callback = () => api.sendMessage({body:``,attachment: fs.createReadStream(__dirname + "/assets/any.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/assets/any.png"),event.messageID);
 	 return request(encodeURI(`https://api.memegen.link/images/slap/${text1}/${text2}.png`)).pipe(fs.createWriteStream(__dirname+'/assets/any.png')).on('close',() => callback());  
 }
