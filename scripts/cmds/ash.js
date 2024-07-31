297a5cd212dfd2f9f642076c04a2babbe22a699a
Divineloo <156040645+Divineloo@users.noreply.github.com>
2024-07-31 11:29:30 +0100

    ash.js

--- scripts/cmds/ash.js
+++ scripts/cmds/ash.js
@@ -13,7 +13,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "yukihira") {
  return message.reply({
- body: "hello, i'm yukihira. follow my other master ASH.",
+ body: "hello, how did you know im ash 😵😅. follow my other master ASH.",
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/yoFQmX2.jpg")
  });
  }
